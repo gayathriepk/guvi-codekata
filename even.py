762c95d6954d07198a484d2826237944817ff7ca
gayathriepk <gayathriepk18@gmail.com>
2018-03-01 19:30:01 +0530

    Add files via upload
    To display even numbers between interval

---
+++ even.py
@@ -0,0 +1,7 @@
+start=int(input("Enter the start range:"))
+end=int(input("Enter the end range:"))
+for i in range(start,end):
+        next=i+1
+        if(next%2==0):
+        	print next
+        	
